a50a62df0bc01aebd45651873157a574739c50c5
Vadym Urvachov <vadym.urvachov@viewnext.com>
2021-07-28 11:08:04 +0200

    Añadir ficheros nuevos.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-#FROM gradle:7.1.1-jdk11 AS base
-#WORKDIR /opt/hello-spring-boot
-#COPY ./ ./
-#RUN ./gradlew build
+FROM gradle:7.1.1-jdk11 AS base
+WORKDIR /opt/hello-spring-boot
+COPY ./ ./
 #RUN ./gradlew assemble
 
-#FROM adoptopenjdk/openjdk11:alpine-jre
-#WORKDIR /opt/hello-spring-boot
-#COPY --from=base /opt/hello-spring-boot/build/libs/demo-0.0.1-SNAPSHOT.jar ./
-#CMD java -jar demo-0.0.1-SNAPSHOT.jar ./
+FROM adoptopenjdk/openjdk11:alpine-jre
+WORKDIR /opt/hello-spring-boot
+COPY --from=base /opt/hello-spring-boot/build/libs/demo-0.0.1-SNAPSHOT.jar ./
+CMD java -jar demo-0.0.1-SNAPSHOT.jar ./
--- src/main/groovy/com/example/demo/DemoApplication.groovy
+++ src/main/groovy/com/example/demo/DemoApplication.groovy
@@ -2,12 +2,21 @@ package com.example.demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@RestController
 class DemoApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run(DemoApplication, args)
+	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "Vadym Urvachov") String name) {
+		return String.format("Proyecto Spring Boot de  %s!", name);
 	}
 
 }
