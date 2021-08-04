1801a52297c5937b4b6de4883d9c07bc748d73c4
Vadym Urvachov <vadym.urvachov@viewnext.com>
2021-08-04 12:22:53 +0200

    Modificamos codigo.

--- src/test/groovy/com/example/demo/HomeControllerTest.java
+++ src/test/groovy/com/example/demo/HomeControllerTest.java
@@ -1,25 +1,10 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.assertj.core.api.Assertions.*;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HomeControllerTest {
-
-    @LocalServerPort
-    public static int port;
-
-    @Autowired
-    public TestRestTemplate restTemplate;
 
     @Test
     void hola(){
-        assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/hola", String.class))
-                .isEqualTo("Buenos dias!");
     }
 }
