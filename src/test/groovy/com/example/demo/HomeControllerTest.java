6b1ec8e46481facce7db2e90a33ee560e8b57fef
Vadym Urvachov <vadym.urvachov@viewnext.com>
2021-08-04 11:57:21 +0200

    Añadiendo pruebas nuevas.

--- src/test/groovy/com/example/demo/HomeControllerTest.java
+++ src/test/groovy/com/example/demo/HomeControllerTest.java
@@ -1,12 +1,26 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HomeControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void hola() {
+        assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/hola", String.class))
+                .isEqualTo("Buenos dias!");
     }
 }
